56578bbff5bcfe163b6b0fcd97e5973200e3865f
chm <502015160@qq.com>
2024-01-15 14:22:58 +0800

    1.15 first commit by company

--- springmvc-file/src/main/java/com/chm/controller/FileController.java
+++ springmvc-file/src/main/java/com/chm/controller/FileController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
@@ -13,12 +14,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.UUID;
 
 @Controller
 public class FileController {
 
-    @RequestMapping("/file/up")
-    public ResponseEntity<byte[]> UpFile(HttpSession session) throws IOException {
+    @RequestMapping("/file/down")
+    public ResponseEntity<byte[]> DownFile(HttpSession session) throws IOException {
         ServletContext servletContext = session.getServletContext();
         String realPath = servletContext.getRealPath("static");
         realPath=realPath+ File.separator+"1.jpg";
@@ -34,5 +36,22 @@ public class FileController {
         return responseEntity;
 
 
+    }
+    @RequestMapping("/file/up")
+    public String UpFile(MultipartFile photo,HttpSession session) throws IOException {
+        ServletContext servletContext = session.getServletContext();
+        String realPath = servletContext.getRealPath("photo");
+        String originalFilename = photo.getOriginalFilename();
+        String hzName = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String fileName = realPath+File.separator+uuid+hzName;
+        File file = new File(realPath);
+        if (!file.exists()){
+            file.mkdir();
+        }
+        photo.transferTo(new File(fileName));
+
+        System.out.println(fileName);
+        return "seccess";
     }
 }
