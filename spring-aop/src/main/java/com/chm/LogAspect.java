f6c0a616c447b69ef5d4fa803786484f59766323
chm <502015160@qq.com>
2024-01-24 09:09:40 +0800

    1.24 first commit by company

--- spring-aop/src/main/java/com/chm/LogAspect.java
+++ spring-aop/src/main/java/com/chm/LogAspect.java
@@ -12,6 +12,12 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class LogAspect {
+//    重用切入点表达式
+    @Pointcut("execution(* com.chm.*.*(..))")
+    public void pointCut(){}
+
+
+
 //    @Before("execution(public int com.chm.CalculatorImpl.add(int,int))")
 //    前置通知
     @Before("execution(* com.chm.*.*(..))")
@@ -45,20 +51,21 @@ public class LogAspect {
         Object[] args = joinPoint.getArgs();
         System.out.println("LogAspect--->异常通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+e);
     }
-
+//    @Around("execution(* com.chm.*.*(..))")
+    @Around("pointCut()")
     public Object aroundMethod(ProceedingJoinPoint joinPoint){
         Signature signature = joinPoint.getSignature();
         Object[] args = joinPoint.getArgs();
         Object result = null;
         try {
-            System.out.println("LogAspect--->前置通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+"返回值为：";
+            System.out.println("LogAspect--->环绕通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args));
             result = joinPoint.proceed();
-            System.out.println("LogAspect--->返回通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+"返回值为：");
+            System.out.println("LogAspect--->环绕通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+"返回值为："+result);
         } catch (Throwable e) {
             System.out.println("LogAspect--->环绕通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+e);
 
         }finally {
-            System.out.println("LogAspect--->返回通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+"返回值为：");
+            System.out.println("LogAspect--->环绕通知，方法名"+signature.getName()+"参数为："+Arrays.toString(args)+"返回值为："+result);
         }
         return result;
     }
